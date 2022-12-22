b93318f20118c3dfd2bee1f88f68e54cf450248b
PavaniNadupalli <117763882+PavaniNadupalli@users.noreply.github.com>
2022-12-22 14:29:40 +0530

    Create PracticeTest.java

---
+++ PracticeTest.java
@@ -0,0 +1,17 @@
+public class PracticeTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String hungry = "yes";
+		
+		if(hungry == "yes")
+		{
+			System.out.println("have a nice food");
+		}
+		else
+		{
+			System.out.println("enjoy");
+		}
+    }
+    }
